b32a6e8fe7d62c5a92bb2a36935dbd8941b9e72a
Josh Steward <stewardjosh10@gmail.com>
2021-02-16 14:05:28 +1100

    server edited

--- Develop/server.js
+++ Develop/server.js
@@ -6,6 +6,9 @@ const app = express();
 //setup port 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
